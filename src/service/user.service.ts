10e985ee6ca518f5404476955367ab6bc6c069fb
gladysdrea <Gladysdrea@gmail.com>
2022-05-11 17:31:50 +0800

    图片更换

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -1,14 +1,28 @@
-import { Provide } from '@midwayjs/decorator';
-import { IUserOptions } from '../interface';
+import { Inject, Provide } from "@midwayjs/decorator";
+import { InjectEntityModel } from '@midwayjs/orm';
+import { UserEntity } from '../entity/user.entity';
+import { Repository } from 'typeorm';
+import { ILogger } from '@midwayjs/logger';
 
 @Provide()
 export class UserService {
-  async getUser(options: IUserOptions) {
-    return {
-      uid: options.uid,
-      username: 'mockedName',
-      phone: '12345678901',
-      email: 'xxx.xxx@xxx.com',
-    };
+  @Inject()
+  logger: ILogger;
+
+  @InjectEntityModel(UserEntity)
+  userModel: Repository<UserEntity>;
+
+  // save
+  async saveUser() {
+    // create a entity object
+    const user = new UserEntity();
+    user.name = 'Gladysdrea';
+    user.password = '12345';
+
+    // save entity
+    const userResult = await this.userModel.save(user);
+
+    // save success
+    this.logger.info(userResult.id);
   }
 }
